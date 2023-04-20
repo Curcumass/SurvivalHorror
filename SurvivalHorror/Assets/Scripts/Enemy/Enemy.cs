edf9d7643c64cc090d5a66a976a8af0ee4cee54f
Oleg Yakonen <hronos1228@yandex.ru>
2023-04-20 17:21:49 +0300

    Improved Enemy script

--- SurvivalHorror/Assets/Scripts/Enemy/Enemy.cs
+++ SurvivalHorror/Assets/Scripts/Enemy/Enemy.cs
@@ -9,17 +9,37 @@ public abstract class Enemy : MonoBehaviour
     [SerializeField] protected Transform _player;
     [SerializeField] protected float _damage;
     [SerializeField] private Transform[] _waypoint;
+    [SerializeField] protected float _viewAngle;
+    [SerializeField] protected float _detectRange;
+    private int _currentPoint = 0;
+
+
 
     protected NavMeshAgent _agent;
 
     private void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
+        _agent.autoBraking = false;
+    }
+
+    protected virtual void DetectPlayer()
+    {
+        Vector3 target = (_player.position - transform.position).normalized;
+        if(Vector3.Angle(transform.forward, target) < _viewAngle)
+        {
+            float distanceToTarget = Vector3.Distance(transform.position, _player.position);
+            if(distanceToTarget < _detectRange)
+            {
+                _agent.stoppingDistance = 7f;
+                Chase();
+            }
+        }
     }
 
     protected virtual void Attack()
     {
-
+        
     }
 
     protected virtual void Chase()
@@ -30,6 +50,11 @@ public abstract class Enemy : MonoBehaviour
 
     protected virtual void Patrol()
     {
-
+        if (_waypoint.Length == 0)
+        {
+            return;
+        }
+        _agent.destination = _waypoint[_currentPoint].position;
+        _currentPoint = (_currentPoint + 1) % _waypoint.Length;
     }
 }
--- SurvivalHorror/Assets/Scripts/Enemy/Gunner.cs
+++ SurvivalHorror/Assets/Scripts/Enemy/Gunner.cs
@@ -9,14 +9,21 @@ public class Gunner : Enemy
     [SerializeField] private float _bulletSpeed = 15f;
 
     [SerializeField] private float _shootCooldown = 1;
+    [SerializeField] private float _shootingDistance = 7f;
     private bool _canShoot = true;
 
 
     private void Update()
     {
-        Chase();
+        if(!_agent.pathPending && _agent.remainingDistance < 0.5f)
+        {
+            Patrol();
+            _agent.stoppingDistance = 0;
+        }
 
-        if(Vector3.Distance(_agent.transform.position, _player.transform.position) <= _agent.stoppingDistance && _canShoot)
+        DetectPlayer();
+
+        if (Vector3.Distance(_agent.transform.position, _player.transform.position) <= _agent.stoppingDistance && _canShoot)
         {
             Attack();
         }
@@ -28,12 +35,12 @@ public class Gunner : Enemy
         GameObject bullet = Instantiate(_bullet, _shootPoint.position, Quaternion.identity);
         Rigidbody bulletRigidbody = bullet.GetComponent<Rigidbody>();
         bulletRigidbody.velocity = transform.forward * _bulletSpeed;
-        
-        StartCoroutine(Shoot());      
+
+        StartCoroutine(Shoot());
     }
 
     private IEnumerator Shoot()
-    {        
+    {
         yield return new WaitForSeconds(_shootCooldown);
         _canShoot = true;
     }
